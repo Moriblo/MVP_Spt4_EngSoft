ff5d2077603bf11eeac91d0eb947fa9190e6ebe8
Moacyr <99156792+Moriblo@users.noreply.github.com>
2023-12-04 20:17:01 -0300

    Versão Operacional com Pytest V1.2

--- back/model/avaliador.py
+++ back/model/avaliador.py
@@ -1,13 +1,34 @@
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+from sklearn.metrics import precision_recall_fscore_support
+import pandas as pd
 
 class Avaliador:
 
-    def avaliar(self, modelo, X_test, Y_test):
-        """ Faz uma predição e avalia o modelo. Poderia parametrizar o tipo de
-        avaliação, entre outros.
+    def avaliar(self, dataset, qtde_lines, atributos, modelo, scaler):
+        """ A partir dos dados do golden data, aplica o modelo e restorna os resultados do 
+        classification report
         """
-        predicoes = modelo.predict(X_test)
-        return (accuracy_score(Y_test, predicoes),
-                recall_score(Y_test, predicoes),
-                precision_score(Y_test, predicoes),
-                f1_score(Y_test, predicoes))
+        SUGESTÃO_CALC = []
+
+        df_carga = list(dataset[atributos].sample(qtde_lines).itertuples(index=False, name=None))
+
+        carga_entrada = pd.DataFrame([tupla[:4] for tupla in df_carga], columns=atributos[:4])
+        carga_saida = pd.DataFrame([tupla[4] for tupla in df_carga], columns=[atributos[-1]])
+
+        array_entrada = carga_entrada.values
+
+        X_entrada = array_entrada[:,0:4].astype(float)
+        rescaledEntradaX = scaler.transform(X_entrada)
+
+        for i in range(qtde_lines):
+            resultado = modelo.predict(rescaledEntradaX)
+
+        SUGESTÃO_CALC.extend(resultado)
+
+        # Obtendo os resultado da aplicação do modelo
+        acuracia = accuracy_score(carga_saida, SUGESTÃO_CALC)
+        precisao, recall, fscore, suporte = precision_recall_fscore_support(carga_saida, SUGESTÃO_CALC)
+
+        precisao_1 = precisao[1]
+
+        return acuracia, precisao_1
--- back/model/modelo.py
+++ back/model/modelo.py
@@ -4,27 +4,12 @@ import joblib
 
 class Model:
     
-    def carrega_modelo(path):
-        """Dependendo se o final for .pkl ou .joblib, carregamos de uma forma ou de outra
-        """
+    def carrega_modelo(self, pathmodel, pathscaler):
+        # Carrega modelo CART
+        with open(pathmodel, "rb") as f:
+            modelo = pickle.load(f)
+        # Carrega scaler utilizado
+        with open(pathscaler, "rb") as s:
+            scaler = pickle.load(s)
         
-        if path.endswith('.pkl'):
-            model = pickle.load(open(path, 'rb'))
-        elif path.endswith('.joblib'):
-            model = joblib.load(path)
-        else:
-            raise Exception('Formato de arquivo não suportado')
-        return model
-    
-    def preditor(model, form):
-        """Realiza a predição de um fundo com base no modelo treinado
-        """
-
-        X_input = np.array([form.RESG_DIA, 
-                            form.CAPTC_DIA, 
-                            form.VL_PATRIM_LIQ, 
-                            form.VL_TOTAL,
-                        ])
-        # Faremos o reshape para que o modelo entenda que estamos passando
-        diagnosis = model.predict(X_input.reshape(1, -1))
-        return int(diagnosis[0])
+        return modelo, scaler
